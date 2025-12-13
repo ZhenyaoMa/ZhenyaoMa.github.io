5520d7bb28f3e96902bda155e2c58a07707cbee4
Zhenyao Ma <154445789+ZhenyaoMa@users.noreply.github.com>
2025-12-13 12:55:32 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,7 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  output: 'export',
-  trailingSlash: true,
-  images: {
-    unoptimized: true,
-  },
-  /* config options here */
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.bib$/,
-      type: 'asset/source',
-    });
-    return config;
-  },
+// next.config.ts
+const nextConfig = {
+  output: "export",
+  images: { unoptimized: true },
 };
 
 export default nextConfig;
